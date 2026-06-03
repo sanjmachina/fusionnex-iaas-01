8249394a22067312225ac4080c00f9dca3885662
Sanjeevi <43089109+sanjmachina@users.noreply.github.com>
2026-06-03 17:48:41 +0530

    Update backend.tf
    updated backend.tf

--- backend.tf
+++ backend.tf
@@ -3,6 +3,7 @@ terraform {
     bucket = "fusionnex-cicd-terraform-state-bucket-01"
     key = "ec2/terraform.tfstate"
     region = "us-east-1"
-    dynamodb_table = "terraform-lock"
+    use_lockfile = true
+    dynamodb_table = "terraform-lock" #<1.10 terraform version - works.
   }
 }
